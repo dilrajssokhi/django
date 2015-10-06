4c326ce54caadba9895dd81b9a7e305f8ddcdc6e
dilrajssokhi <dilrajssokhi@gmail.com>
2015-10-07 00:42:23 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^urlShortner/', include('urlShortner.urls', namespace="urlShortner")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^movies/', include('movies.urls', namespace="movies")),
 ]
